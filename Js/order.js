82d0d58908303340be83e55f7367dafb05f23aad
Ernestobolkin <ernest1047@gmail.com>
2021-11-28 08:57:08 +0200

    Almost finished

--- Js/order.js
+++ Js/order.js
@@ -1,10 +1,30 @@
-let count = document.querySelector('.zero');
-const inc = document.querySelector('.addTo');
-// const qty = document.querySelector('')
-inc.addEventListener('click', () => {
-  if(count.textContent<9){
+
+// selector testing
+const qty = document.querySelector("select");
+const op = document.querySelector("option");
+
+
+  let selectval = document.querySelector("select").value;
+  console.log(selectval);
+// function getSelectOp(selectval){
+//   let opt;
+//   for(let i = 0; )
+// }
+
+// console.log(qty.options[qty.selectedIndex]);
+op.addEventListener("click", () => {
+  console.log("working")
+});
+
+
+
+// working
+let count = document.querySelector(".zero");
+const inc = document.querySelector(".addTo");
+inc.addEventListener("click", () => {
+  if (count.textContent < 9) {
     count.textContent++;
-  }else{
-    alert("you cant have more then 9 items. sorry")
+  } else {
+    alert("you cant have more then 9 items. sorry");
   }
 });
